c37823685dd7a51b4ab8c369533614e78a4c6e9a
Sri Krishna Kaashyap M <30852667+srikrishnakaashyap@users.noreply.github.com>
2023-09-30 14:06:59 -0400

    Updates

---
+++ static/js/8.6fce7429.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpportfolio-app"]=this["webpackJsonpportfolio-app"]||[]).push([[8],{86:function(t,e,p){"use strict";p.r(e);p(0),p(88);var a=p(1);e.default=function(t){var e=t.title;return Object(a.jsx)("div",{className:"header",children:e})}}}]);
+//# sourceMappingURL=8.6fce7429.chunk.js.map
